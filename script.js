edb80731ed33e1dcf5e316fb0753204e12216094
Lorenzo Marinucci <lorenmarinucci@gmail.com>
2020-01-07 20:40:16 -0300

    CSS añadido.

---
+++ script.js
@@ -0,0 +1,9 @@
+const container=document.querySelector('#container');
+      style=document.querySelectorAll('.color');
+      clear=document.querySelector('#clear');
+      resize=document.querySelector('#resize');
+      CONTAINER_SIZE=400;
+
+window.addEventListener('load', () => {
+    alert('loaded');
+})
