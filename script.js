d0a4a2d42494a9d6627557cee7598f13e4ebe082
Lorenzo Marinucci <lorenmarinucci@gmail.com>
2020-01-07 21:19:48 -0300

    Generacion inicial de bloques añadida.

--- script.js
+++ script.js
@@ -4,6 +4,25 @@ const container=document.querySelector('#container');
       resize=document.querySelector('#resize');
       CONTAINER_SIZE=400;
 
+function resizeCanvas(squares){
+    const SQUARE_SIZE=CONTAINER_SIZE/squares;
+    let div;
+   // for (let i=1; i<=Math.pow(squares, 2); i++) {
+    for (let i=1; i<=squares; i++) {
+        let row=document.createElement('div');
+        row.classList.add('row');
+        row.setAttribute('style', `width: ${SQUARE_SIZE*squares}px; height: ${SQUARE_SIZE}px;`)
+        for (let j=1; j<=squares; j++) {
+            div=document.createElement('div');
+            div.classList.add('block');
+            div.setAttribute('style', `width: ${SQUARE_SIZE}px; height: ${SQUARE_SIZE}px; background-color:white;`);
+            row.appendChild(div);
+         }
+        container.appendChild(row);
+    }
+}
+
 window.addEventListener('load', () => {
     alert('loaded');
-})
+    resizeCanvas(16);
+});
